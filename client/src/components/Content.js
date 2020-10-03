8192b1f1365f73f9363c19cc4eb45487482dbb42
amreesalmonte <amreesalmonte@gmail.com>
2020-10-02 22:42:11 -0600

    added button functionality

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -17,32 +17,68 @@ export default class Heading extends Component {
         this.onClickCopy = this.onClickCopy.bind(this);
         this.onClickDel = this.onClickDel.bind(this);
         this.onChangeTranscript = this.onChangeTranscript.bind(this);
+        this.handleListen = this.handleListen.bind(this);
 
         this.state = {
             listening: false,
+            showMessage: false,
             transcript: ""
         }
     }
 
     onClickRec() {
+        // starts or stops recording
+
         this.setState({
             listening: !this.state.listening
-        })
+        }, this.handleListen)
     }
 
-    onClickCopy() {
-        console.log("copy");
+    onClickCopy(e) {
+        // copies text to clipboard
+
+        this.textArea.select();
+        document.execCommand("copy")
     }
 
     onClickDel() {
-        console.log("del");
+        // deletes text field
+        this.setState({
+            transcript: ""
+        })
     }
 
     onChangeTranscript(e) {
+        // handles change state of textarea
         this.setState({
             transcript: e.target.value
         })
         console.log(this.state.transcript)
+    }
+
+    handleListen() {
+        // handles event listener
+        if (this.state.listening) {
+            recognition.start()
+            recognition.addEventListener('end', recognition.start)
+            console.log("start")
+        } else {
+            recognition.stop()
+            console.log("end")
+        }
+
+        let finalTranscript = "";
+        recognition.onresult = event => {
+            const transcript = Array.from(event.results)
+                .map(result => result[0])
+                .map(result => result.transcript)
+                .join("")
+
+            if (event.results[0].isFinal) {
+                finalTranscript += transcript + " "
+            }
+            console.log(finalTranscript)
+        }
     }
 
     render() {
@@ -57,7 +93,12 @@ export default class Heading extends Component {
                     </div>
                 </div>
                 <div>
-                    <textarea className="Note-Content" value={this.state.transcript} onChange={this.onChangeTranscript} />
+                    <textarea
+                        ref={(textarea) => this.textArea = textarea}
+                        className="Note-Content"
+                        value={this.state.transcript}
+                        onChange={this.onChangeTranscript}
+                        id="transcription" />
                 </div>
             </div>
         );
